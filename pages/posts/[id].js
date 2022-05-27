cedef050c8a0113d54979b47f6a075d8cbcdd521
Tibor Lancsak <tibor.lancsak@pa-ats.com>
2022-05-27 22:22:58 +0200

    id.js, posts.js adjustment

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,46 +1,39 @@
-// a dynamic post page for all possible [id]s
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
-import Layout from '../../components/layout';
-import { Head } from 'next/document';
-
-// activate funtions from the library
-import { getAllPostIds, getPostData } from '../../lib/posts';
-
-// return the IDs as props for the Post function
-export async function getStaticPaths() {
-  const paths = getAllPostIds();
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-// return the post data as props for the Post function
-// turned to async due to remark being async in getPostData
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
-  return {
-    props: {
-      postData,
-    },
-  };
-}
-
-// use postData in the page display
-// display post content with Html renderer
 export default function Post({ postData }) {
   return (
     <Layout>
       <Head>
         <title>{postData.title}</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
-  );
+  )
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
 }
